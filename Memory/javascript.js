abc3985e33f2a87298210eeb9e1a9dd3ca6eb9d1
KimberlyDelgado <kdelgado1@kippnyccp.org>
2015-03-11 11:19:10 -0400

    Update javascript.js

--- Memory/javascript.js
+++ Memory/javascript.js
@@ -1,18 +1,17 @@
-var onfirstclick=true;
 $(document).ready(function(){
-  for(i=0; i<20; i++){
-    $('#card_holder').append('<div class="card"><p>'+i+'</p></div>');
- }
-$("div.card"). click(function(){
-  var mynum=0;
-  var firstcard,secondcard;
-  $(this).find('p').css("opacity","1");
-    if(!onfirstclick){
-    //compare cards
-  } else{
-      firstcard=$(this).find('p').html();
-      console.log(firstcard);
+  var card_array = ["fish", "fish", "turtle", "turtle", "pig", "pig", "dog", "dog", "cat", "cat", "snake", "snake", "bird", "bird", "goat", "goat", "hamster", "hamster", "dragon", "dragon"];
+
+  for(i in card_array){
+    $('#card_holder').append('<div class="card"><p>'+card_array[i]+'</p></div>');
+  };
+  var clicked = false;
+  $('.card').on(function(){
+    if(on == false){
+      $(this).find('p').css('opacity', 1);
+      on = true;
     }
-   }
+    else {
+       $(this).find('p').css('opacity',0);
+    }
   });
 });
