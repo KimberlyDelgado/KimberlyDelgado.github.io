31911438165d6ecf10dc656a42d528d6a615a914
KimberlyDelgado <kdelgado1@kippnyccp.org>
2015-03-11 09:15:05 -0400

    Update javascript.js

--- Memory/javascript.js
+++ Memory/javascript.js
@@ -1,11 +1,8 @@
 var onfirstclick=true;
 $(document).ready(function(){
-  var card_array = ["fish", "fish", "turtle", "turtle", "pig", "pig", "dog", "dog", "cat", "cat", "snake", "snake", "bird", "bird", "goat", "goat", "hamster", "hamster", "dragon", "dragon"]
-
-  for(i in card_array){
-    $('#card_holder').append('<div class="card"><p>'+card_array[i]+'</p></div>');
+  for(i=0; i<20; i++){
+    $('#card_holder').append('<div class="card"><p>'+i+'</p></div>');
   }
-});
 $("div.card"). click(function(){
   var mynum=0;
   var firstcard,secondcard;
