3a7a7b4ae1768df7d09334fcf6b1e50efe87793d
KimberlyDelgado <kdelgado1@kippnyccp.org>
2015-03-12 08:59:18 -0400

    Update javascript.js

--- Memory/javascript.js
+++ Memory/javascript.js
@@ -1,18 +1,11 @@
 $(document).ready(function(){
-  var card_array = ["fish", "fish", "turtle", "turtle", "pig", "pig", "dog", "dog", "cat", "cat", "snake", "snake", "bird", "bird", "goat", "goat", "hamster", "hamster", "dragon", "dragon"]
+ var click_count=0
+ var first_guess=
+ var 
+ var
+  var card_array = ["1", "1", "2", "2", "3", "3", "4", "4", "5", "5", "6", "6", "7", "7", "8", "8", "9", "9", "10", "10"]
 
   for(i in card_array){
     $('#card_holder').append('<div class="card"><p>'+card_array[i]+'</p></div>');
- var clicked = false;
-  $('.card').on('click'; function(){
-    if(on == false){
-      $(this).find('p').css('opacity', 1);
-      on = true;
-    }
-    else {
-       $(this).find('p').css('opacity',0);
-    }
-  });
-});
-
+ 
 
