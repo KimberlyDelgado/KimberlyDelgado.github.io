2ee1b4221369d92d54e5a7c7aab2c4efbb980641
KimberlyDelgado <kdelgado1@kippnyccp.org>
2015-03-12 08:31:20 -0400

    Update javascript.js

--- Memory/javascript.js
+++ Memory/javascript.js
@@ -1,17 +1,7 @@
 $(document).ready(function(){
-  var card_array = ["1", "1", "2", "2", "3", "3", "4", "4", "5", "5", "6", "6", "7", "7", "8", "8", "9", "9", "10", "dragon"];
+  var card_array = ["1", "1", "2", "2", "3", "3", "4", "4", "5", "5", "6", "6", "7", "7", "8", "8", "9", "9", "10", "10"];
 	$("div.card"). click(function(){
   for(i in card_array){
     $('#card_holder').append('<div class="card"><p>'+card_array[i]+'</p></div>');
   };
-  var clicked = false;
-  $('.card').on(function(){
-    if(on == false){
-      $(this).find('p').css('opacity', 1);
-      on = true;
-    }
-    else {
-       $(this).find('p').css('opacity',0);
-    }
-  });
-});
+ 
