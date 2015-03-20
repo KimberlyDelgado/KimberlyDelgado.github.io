822a98b5701ab0a16d46da65607ad0774c588ad1
KimberlyDelgado <kdelgado1@kippnyccp.org>
2015-03-20 08:57:23 -0400

    Update javascript.js

--- ShoppingList/javascript.js
+++ ShoppingList/javascript.js
@@ -3,7 +3,7 @@ $(document).ready(function(){
    var item=$('input').val();
    $("div").append('<p>'+item+'</p>')
   });
-  $('Remove').on('click',function(){
+ // $('Remove').on('click',function(){
     var item=$('input').val();
  //   $('div').empty();
 })
