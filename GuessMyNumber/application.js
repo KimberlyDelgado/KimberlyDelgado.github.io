13745036a7826858e9a091076701d5a3616a5ec0
KimberlyDelgado <kdelgado1@kippnyccp.org>
2015-03-31 09:03:37 -0400

    Update application.js

--- GuessMyNumber/application.js
+++ GuessMyNumber/application.js
@@ -6,24 +6,24 @@ $(document).ready(function(){
   $('button').on('click', function(){
     //save their guess as a variable
     var userGuess= $('input').val();
-    
+      
     
     //and compare this guess to the random number that the computer picked
     //if the user guessed the correct number...
     if (userGuess === comp_Integer){
-      return "Congratulations! You guessed the right number!";
+      confirm ("Congratulations! You guessed the right number!");
     }
       //what happens if the guess is correct?
       
     //if the user guessed too high...
     else if (userGuess > comp_Integer){
-      return "Your guess is too high! Guess a lower number!";
+      confirm ("Your guess is too high! Guess a lower number!");
     }
       //update the 'feedback' paragraph to tell them to guess lower
       
     //otherwise, the user guessed too low...
     else{
-      return "Your guess is too low! Guess higher!";
+      confirm ("Your guess is too low! Guess higher!");
     }
       //update the 'feedback' paragraph to tell them to guess higher
     
