6291e9b8ffc5f624e7383fbe66bdbd2932fd6233
KimberlyDelgado <kdelgado1@kippnyccp.org>
2015-04-02 08:46:24 -0400

    Update application.js

--- GuessMyNumber/application.js
+++ GuessMyNumber/application.js
@@ -11,7 +11,7 @@ $(document).ready(function(){
       
       
       var element = document.getElementById("guess_counter");
-      element.innerHTML = "Number of Guesses="+guessNum;
+      element.innerHTML = guessNum = + "Number of Guesses=";
     
     //and compare this guess to the random number that the computer picked
     //if the user guessed the correct number...
