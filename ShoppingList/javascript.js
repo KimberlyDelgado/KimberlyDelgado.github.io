6ecace76080cd959fc503e2a91dfdda9d55edf4d
KimberlyDelgado <kdelgado1@kippnyccp.org>
2015-04-15 09:05:40 -0400

    Update javascript.js

--- ShoppingList/javascript.js
+++ ShoppingList/javascript.js
@@ -1,11 +1,11 @@
 $(document).ready(function(){
-  $('#add').on('click',function(){
-   var item=$('input').val();
-   $("div").append('<p>'+item+'</p>')
+  $('#add').on('click',function(){//this makes the button add when you click it
+   var item=$('input').val();// this keeps track of the word that you put in the input box
+   $("div").append('<p>'+item+'</p>')//this adds the word to the div
   });
-  $('#remove').on('click',function(){
-    var item=$('input').val();
-   $('div').empty();
+  $('#remove').on('click',function(){//this makes the button remove the items in the div
+    var item=$('input').val();//this keep track of what is in the input
+   $('div').empty();//this clears the whole div
 })
 }
 )
